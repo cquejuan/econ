c26a3600e5bb5d16a636c0c32f68057e82f9210d
cquejuan <58236313+cquejuan@users.noreply.github.com>
2019-11-26 21:43:15 -0900

    Update al.js

--- al.js
+++ al.js
@@ -1,12 +1,17 @@
 
 var cont = document.createElement("DIV");
 cont.class = "container";
+//cont.class.width = "600px";
+//cont.class.height = "800px";
 var btns = [
   {
     "name" : "Create Deductible",
     "function" : "deductible('new')"
   }
 ];
+for(i = 0; i < btns.length; i++){
+  btns[i].id = "btn-" + i;
+}
 function deductible(x){
   if(x = 'new'){
     var y = document.createElement("INPUT");
