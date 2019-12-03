15f92badf026fd656251352a83d40f38ebeb54ac
cquejuan <58236313+cquejuan@users.noreply.github.com>
2019-12-02 21:17:50 -0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,15 @@
 //Compound Interests
+//A = a * (1 + r/n)^nt 
+/*
+A = a * (1 + r/n)^nt
+=> A = a * Math.pow((1 + r/n), n * t);
+
+a = A / (1 + r/n)^nt
+=> a = A / Math.pow(1 + r/n)^nt
+
+t = ln(A) / (nln(a * (n + r) / n))
+=> t = Math.ln(a) / (n * Math.ln(a * (n + r) / n))
+*/
+function cInterest(a, A, n, r, t){
+
+}
